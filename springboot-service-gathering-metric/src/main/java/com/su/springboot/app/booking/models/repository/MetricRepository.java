a433ebd4c7f5b477210254d3960c7b5c23c9967b
hectorhans <hector.romerosandon@gmail.com>
2020-09-24 21:44:08 -0500

    deleting older project

--- springboot-service-gathering-metric/src/main/java/com/su/springboot/app/booking/models/repository/MetricRepository.java
+++
@@ -1,9 +0,0 @@
-package com.su.springboot.app.booking.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.su.springboot.app.booking.models.entity.Metric;
-
-public interface MetricRepository extends CrudRepository<Metric, Long> {
-
-}
